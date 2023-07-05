685d25e5fa794a55065a0b6e7af2ee68fb30edb8
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-07-05 13:20:54 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<template>
+  <h1 class="text-danger">TEST</h1>
+</template>
+
+<style lang="scss">
+@use './assets/sass/style.scss'
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+library.add(faStar);
+import App from './App.vue';
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
