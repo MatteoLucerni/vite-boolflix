42d7cd3c7188324f909695cdbbdb85637c480aca
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-07-05 13:47:58 +0200

    added SearchBar component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,18 @@
 <script>
 import { store } from '../data/store'
+import AppSearchBar from './AppSearchBar.vue'
 export default {
+    components: {
+        AppSearchBar
+    },
     data() {
         return {
-            userSearch: store.userSearch
+
+        }
+    },
+    methods: {
+        saveUserSearch(string) {
+            store.userSearch = string
         }
     }
 }
@@ -15,10 +24,7 @@ export default {
             <div class="logo fs-1 fw-bold text-danger">
                 Netflix
             </div>
-            <div class="d-flex">
-                <input v-model.trim="userSearch" class="form-control" type="text" placeholder="Cerca film o serie tv">
-                <button class="btn btn-primary ms-2">CERCA</button>
-            </div>
+            <AppSearchBar @button-clicked="saveUserSearch" />
         </nav>
     </div>
 </template>
---
+++ src/components/AppSearchBar.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data() {
+        return {
+            searchText: ''
+        }
+    },
+    emits: ['button-clicked']
+}
+
+</script>
+
+<template>
+    <div class="d-flex">
+        <input v-model.trim="searchText" class="form-control" type="text" placeholder="Cerca film o serie tv">
+        <button @click="$emit('button-clicked', searchText)" class="btn btn-primary ms-2">CERCA</button>
+    </div>
+</template>
